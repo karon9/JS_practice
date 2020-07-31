48c2db1036e7ec1d4782665ff6c3ad4bf10cbf57
charon <karon9@users.noreply.github.com>
2020-07-31 13:31:47 +0900

    国旗当てクイズ

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function showSum() {
-    var num1 = 1;
-    var num2 = 2;
-    var sum = num1 + num2;
-    console.log(event);
-    alert(sum);
+function show() {
+    alert("不正解");
+}
+function show1() {
+    alert("不正解");
+}
+function show2() {
+    alert("不正解");
+}
+function show3() {
+    alert("正解");
+}
+function show4() {
+    alert("不正解");
 }
